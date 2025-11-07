f80becd8e539e8571c9f434ed46e5077cae7ef53
nicerobot <nicerobot@users.noreply.github.com>
2025-11-07 15:47:36 -0800

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+# yup-tee
+
+```
+NAME:
+   tee - read from standard input and write to standard output and files
+
+USAGE:
+   tee [OPTIONS] [FILE...]
+
+      Copy standard input to each FILE, and also to standard output.
+
+COMMANDS:
+   help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --append, -a             append to the given FILEs, do not overwrite (default: false)
+   --ignore-interrupts, -i  ignore interrupt signals (default: false)
+   --help, -h               show help
+```
---
+++ yup-tee/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli/v2"
+
+	yup "github.com/gloo-foo/framework"
+	. "github.com/yupsh/tee"
+)
+
+const (
+	flagAppend          = "append"
+	flagIgnoreInterrupt = "ignore-interrupts"
+)
+
+func main() {
+	app := &cli.App{
+		Name:  "tee",
+		Usage: "read from standard input and write to standard output and files",
+		UsageText: `tee [OPTIONS] [FILE...]
+
+   Copy standard input to each FILE, and also to standard output.`,
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:    flagAppend,
+				Aliases: []string{"a"},
+				Usage:   "append to the given FILEs, do not overwrite",
+			},
+			&cli.BoolFlag{
+				Name:    flagIgnoreInterrupt,
+				Aliases: []string{"i"},
+				Usage:   "ignore interrupt signals",
+			},
+		},
+		Action: action,
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintf(os.Stderr, "tee: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func action(c *cli.Context) error {
+	var params []any
+
+	// Add file arguments
+	for i := 0; i < c.NArg(); i++ {
+		params = append(params, yup.File(c.Args().Get(i)))
+	}
+
+	// Add flags based on CLI options
+	if c.Bool(flagAppend) {
+		params = append(params, Append)
+	}
+	if c.Bool(flagIgnoreInterrupt) {
+		params = append(params, IgnoreInterrupt)
+	}
+
+	// Create and execute the tee command
+	cmd := Tee(params...)
+	return yup.Run(cmd)
+}
