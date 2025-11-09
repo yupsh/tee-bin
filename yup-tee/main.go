f6b7f2c865ab3159774d599c8c2664aa195cfc64
nicerobot <nicerobot@users.noreply.github.com>
2025-11-08 17:51:08 -0800

    tests

--- yup-tee/main.go
+++ yup-tee/main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/urfave/cli/v2"
 
-	yup "github.com/gloo-foo/framework"
+	gloo "github.com/gloo-foo/framework"
 	. "github.com/yupsh/tee"
 )
 
@@ -48,7 +48,7 @@ func action(c *cli.Context) error {
 
 	// Add file arguments
 	for i := 0; i < c.NArg(); i++ {
-		params = append(params, yup.File(c.Args().Get(i)))
+		params = append(params, gloo.File(c.Args().Get(i)))
 	}
 
 	// Add flags based on CLI options
@@ -61,5 +61,5 @@ func action(c *cli.Context) error {
 
 	// Create and execute the tee command
 	cmd := Tee(params...)
-	return yup.Run(cmd)
+	return gloo.Run(cmd)
 }
